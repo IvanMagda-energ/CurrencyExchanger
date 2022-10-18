b3d8fe0e26fe8e2a75292f40f294b480e048010e
Ivan Magda <magda.vano@gmail.com>
2022-10-18 19:51:26 +0200

    nav graph created + basic dark/light themes configured

--- app/src/main/java/com/cml/currencyexchanger/view/activities/MainActivity.kt
+++ app/src/main/java/com/cml/currencyexchanger/view/activities/MainActivity.kt
@@ -1,7 +1,9 @@
-package com.cml.currencyexchanger
+package com.cml.currencyexchanger.view.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
+import com.cml.currencyexchanger.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
