18ee2dbabafb2eec96ea2432a0a5e4e4ff7da468
Ivan Magda <magda.vano@gmail.com>
2022-10-19 04:20:21 +0200

    viewbinding added + small ui fixes

--- app/src/main/java/com/cml/currencyexchanger/view/fragments/CurrencyConverterFragment.kt
+++ app/src/main/java/com/cml/currencyexchanger/view/fragments/CurrencyConverterFragment.kt
@@ -1,19 +1,14 @@
 package com.cml.currencyexchanger.view.fragments
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
-import androidx.fragment.app.Fragment
-import com.cml.currencyexchanger.R
+import com.cml.currencyexchanger.databinding.FragmentCurrencyConverterBinding
 
-class CurrencyConverterFragment : Fragment() {
+class CurrencyConverterFragment :
+    BaseFragment<FragmentCurrencyConverterBinding>(FragmentCurrencyConverterBinding::inflate) {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_currency_converter, container, false)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
     }
 
 }
